ff21ef7608d777be511279ddce5f213bf4fece86
LuizZonetti1 <luizfbzonetti@gmail.com>
2026-02-05 14:50:22 -0300

    servidor inicializado

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import routes from "./routes";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use((err: Error, req: any, res: any, next: any) => {
+	console.log(err);
+	res.status(400).json({
+		message: err.message || "Erro inesperado.",
+	});
+});
+
+export default app;
